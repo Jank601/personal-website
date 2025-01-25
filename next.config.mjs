3e604692da6b138975dab329f70238961a6ae8c1
Eli Yagel <eli.yagel.work@gmail.com>
2025-01-25 19:39:12 +0200

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    REACT_APP_GA_TRACKING_ID: process.env.REACT_APP_GA_TRACKING_ID,
+  },
 };
 
 export default nextConfig;
